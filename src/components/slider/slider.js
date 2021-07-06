19a58aef3e0689e83ff6181a0122444abcdec6bb
alskra <krasnoyurchenko.a.s@gmail.com>
2021-07-06 18:08:51 +0300

    update

---
+++ src/components/slider/slider.js
@@ -0,0 +1,7 @@
+import 'swiper/swiper-bundle.css';
+import Swiper from 'swiper';
+import './slider.scss';
+
+const swiper = new Swiper('.swiper-container');
+
+export default swiper;
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,6 +2,7 @@ import Alpine from 'alpinejs';
 import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
+import '../../components/slider/slider';
 import '../../components/app/app';
 
 Alpine.start();
