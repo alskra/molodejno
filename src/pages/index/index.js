cb0adc590c6d0cab3483b32f782fde1ce8062bdd
alskra <krasnoyurchenko.a.s@gmail.com>
2021-07-08 14:13:54 +0300

    add (promo)

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,6 +3,7 @@ import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
 import '../../components/slider/slider';
+import '../../components/promo/promo';
 import '../../components/app/app';
 
 Alpine.start();
