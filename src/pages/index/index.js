a6e985f7f81cf71dc3cc36c332211c003f88439e
alskra <krasnoyurchenko.a.s@gmail.com>
2021-07-17 15:03:55 +0300

    update (quote)

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,6 +4,7 @@ import '../../components/header/header';
 import '../../components/footer/footer';
 import '../../components/slider/slider';
 import '../../components/promo/promo';
+import '../../components/quote/quote';
 import '../../components/app/app';
 
 Alpine.start();
