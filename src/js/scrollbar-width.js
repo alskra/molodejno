fc2d9266d76f46e9701b85932d0e0ea8fe35a6b8
alskra <krasnoyurchenko.a.s@gmail.com>
2021-07-19 06:41:37 +0300

    update

--- src/js/scrollbar-width.js
+++ src/js/scrollbar-width.js
@@ -1,7 +1,10 @@
 import ResizeObserver from 'resize-observer-polyfill';
 
-const resizeObserver = new ResizeObserver(() =>
-	document.documentElement.style
-		.setProperty('--scrollbar-width', `${innerWidth - document.body.clientWidth}px`));
+const setScrollbarWidth = () => document.documentElement.style
+	.setProperty('--scrollbar-width', `${innerWidth - document.body.clientWidth}px`);
+
+setScrollbarWidth();
+
+const resizeObserver = new ResizeObserver(setScrollbarWidth);
 
 resizeObserver.observe(document.body);
