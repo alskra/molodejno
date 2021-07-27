ed7325392354495186a669c1b00397c56af28ff2
alskra <krasnoyurchenko.a.s@gmail.com>
2021-07-27 17:17:49 +0300

    update

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,2 +1,51 @@
+import Alpine from 'alpinejs';
+import 'swiper/swiper-bundle.css';
+import Swiper from 'swiper';
 import '../project-item/project-item';
 import './projects.scss';
+
+Alpine.data('projects', () => ({
+	swiperIsInit: false,
+	get isDesktop() {
+		return this.$store.isDesktop;
+	},
+	init() {
+		this.swiper = new Swiper(this.$refs.swiperContainer, {
+			init: false,
+			slidesPerView: 'auto',
+		});
+
+		const swiper = () => {
+			if (!this.isDesktop) {
+				this.initSwiper();
+			} else {
+				this.destroySwiper();
+			}
+		};
+
+		swiper();
+		this.$watch('isDesktop', swiper);
+	},
+	destroy() {
+		Object.defineProperty(this, 'isDesktop', {value: false});
+		this.destroySwiper();
+	},
+	initSwiper() {
+		if (!this.swiperIsInit) {
+			this.swiper.on('init', () => {
+				this.swiper.destroyed = false;
+				this.swiperIsInit = true;
+			});
+			this.swiper.on('destroy', () => {
+				this.swiper.destroyed = true;
+				this.swiperIsInit = false;
+			});
+			this.swiper.init();
+		}
+	},
+	destroySwiper() {
+		if (this.swiperIsInit) {
+			this.swiper.destroy(false);
+		}
+	},
+}));
