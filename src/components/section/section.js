faed2c45b1a4bee2ee39b5b34ec3853402e62838
alskra <krasnoyurchenko.a.s@gmail.com>
2021-07-29 18:04:20 +0300

    update (dist)

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -5,6 +5,10 @@ import '../projects/projects';
 import './section.scss';
 
 // eslint-disable-next-line
-const rellax = new Rellax('.section__rellax', {
+// const rellax = new Rellax('.section__rellax', {
+// 	center: true,
+// });
+
+document.addEventListener('DOMContentLoaded', () => setTimeout(() => new Rellax('.section__rellax', {
 	center: true,
-});
+})));
