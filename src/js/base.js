84b9ad64e1f3a6d097fd32793150a13ba644e7eb
alskra <krasnoyurchenko.a.s@gmail.com>
2021-09-23 11:10:43 +0300

    update

--- src/js/base.js
+++ src/js/base.js
@@ -18,6 +18,7 @@ import '../components/heading/heading.scss';
 import '../components/date/date.scss';
 import '../components/location/location.scss';
 import '../components/button-arrow/button-arrow';
+import '../components/button-load/button-load';
 import '../components/integration/integration.scss';
 import '../components/breadcrumbs/breadcrumbs.scss';
 
