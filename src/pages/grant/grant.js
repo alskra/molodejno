89f2d902f3d5ea3749b8038bc4334c6be20cf3b5
alskra <krasnoyurchenko.a.s@gmail.com>
2021-09-26 20:54:13 +0300

    update

--- src/pages/grant/grant.js
+++ src/pages/grant/grant.js
@@ -3,6 +3,7 @@ import Alpine from 'alpinejs';
 import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
+import '../../components/article/article';
 import '../../components/subsection/subsection';
 import '../../components/app/app';
 
