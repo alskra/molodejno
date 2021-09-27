e2dec68f4f282e695582709ffd514c043acc16a5
alskra <krasnoyurchenko.a.s@gmail.com>
2021-09-27 15:36:16 +0300

    update

--- src/pages/grant/grant.js
+++ src/pages/grant/grant.js
@@ -5,6 +5,7 @@ import '../../components/header/header';
 import '../../components/footer/footer';
 import '../../components/article/article';
 import '../../components/article-nav/article-nav';
+import '../../components/card-details/card-details';
 import '../../components/subsection/subsection';
 import '../../components/app/app';
 
