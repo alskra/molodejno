a94ed6d9675a1173373a076541da543aa3e8a4c6
alskra <krasnoyurchenko.a.s@gmail.com>
2021-09-29 15:06:10 +0300

    update

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,7 +2,7 @@ import Alpine from 'alpinejs';
 
 import '../../js/base';
 // import '../../components/header/header';
-import '../../components/footer/footer';
+// import '../../components/footer/footer';
 import '../../components/app/app';
 
 try {
