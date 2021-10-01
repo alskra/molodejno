3b303157f60035c94695fa58e677b321a365d922
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-01 14:18:27 +0300

    update

--- src/pages/recipient/recipient.js
+++ src/pages/recipient/recipient.js
@@ -4,9 +4,12 @@ import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
 
+import '../../components/gallery/gallery';
+
 import '../../components/recipient-item/recipient-item';
 import '../../components/article-nav/article-nav';
 import '../../components/gallery-slider/gallery-slider';
+import '../../components/subsection/subsection';
 
 import '../../components/section/section';
 import '../../components/app/app';
