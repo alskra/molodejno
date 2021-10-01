782dfe12f164d2cdee4f7b98941f04573b679f54
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-01 10:55:45 +0300

    update

--- src/components/article-nav/article-nav.js
+++ src/components/article-nav/article-nav.js
@@ -9,13 +9,13 @@ Alpine.data('articleNav', () => ({
 
 		sections = sections.filter((item) => ids.includes(`#${item.id}`));
 
-		const boder = document.querySelector('.header').getBoundingClientRect().bottom;
+		const border = 0;
 		let currentSection;
 
 		for (let i = 0; i < sections.length; i++) {
 			const rect = sections[i].getBoundingClientRect();
 
-			if (rect.top <= boder) {
+			if (Math.floor(rect.top) <= border) {
 				currentSection = sections[i];
 			}
 		}
--- src/pages/recipient/recipient.js
+++ src/pages/recipient/recipient.js
@@ -4,6 +4,9 @@ import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
 
+import '../../components/recipient-item/recipient-item';
+import '../../components/article-nav/article-nav';
+
 import '../../components/section/section';
 import '../../components/app/app';
 
