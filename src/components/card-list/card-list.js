209e34c1b1a0ddb9cd7d2c861b632d57330f3dce
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-05 12:55:05 +0300

    update

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -1,3 +1,3 @@
 import '../swiper-cards/swiper-cards';
 import '../card-item/card-item';
-import './projects.scss';
+import './card-list.scss';
--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,7 +1,7 @@
 import Rellax from 'rellax';
 import '../score/score';
 import '../events/events';
-import '../projects/projects';
+import '../card-list/card-list';
 import './section.scss';
 
 window.addEventListener('load', () => new Rellax('.section__rellax', {
