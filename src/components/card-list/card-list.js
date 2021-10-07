f8c3eb4af379eb7d6767bf1e71793833a683f11a
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-08 00:49:37 +0300

    update

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -1,3 +1,4 @@
 import '../swiper-cards/swiper-cards';
 import '../card-item/card-item';
+import '../pagination/pagination';
 import './card-list.scss';
--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,1 +1,19 @@
+import Alpine from 'alpinejs';
 import './pagination.scss';
+
+Alpine.data('pagination', (url, containerEl) => ({
+	error: null,
+	url,
+	containerEl,
+	async load() {
+		const response = await fetch(this.url);
+
+		if (response.ok) {
+			const html = (await response.text()).trim();
+
+			this.containerEl.insertAdjacentHTML('beforeend', html);
+		} else {
+			this.error = `HTTP Error: ${response.status}`;
+		}
+	},
+}));
