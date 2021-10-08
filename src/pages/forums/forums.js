37628dead96f0546aba40940112a150f9833f8a0
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-08 17:34:33 +0300

    update

---
+++ src/pages/forums/forums.js
@@ -0,0 +1,14 @@
+import Alpine from 'alpinejs';
+
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/filters/filters';
+
+import '../../components/section/section';
+
+import '../../components/app/app';
+
+Alpine.start();
