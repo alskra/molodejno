4eeb86d92c7695267a349104b026416302bb719d
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-11 12:22:22 +0300

    update

---
+++ src/pages/forum/forum.js
@@ -0,0 +1,25 @@
+import Alpine from 'alpinejs';
+
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/gallery-slider/gallery-slider';
+import '../../components/stages/stages';
+import '../../components/docs/docs';
+import '../../components/cards/cards';
+import '../../components/gallery/gallery';
+import '../../components/reviews/reviews';
+
+import '../../components/subsection-section/subsection-section';
+
+import '../../components/article-nav/article-nav';
+import '../../components/card-details/card-details';
+import '../../components/subsection/subsection';
+
+import '../../components/article/article';
+
+import '../../components/app/app';
+
+Alpine.start();
