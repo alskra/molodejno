6939e629aaa6f4e99a31ec877ef45192edf41c07
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-12 12:58:35 +0300

    update

--- src/pages/forum/forum.js
+++ src/pages/forum/forum.js
@@ -5,6 +5,7 @@ import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
 
+import '../../components/tags/tags';
 import '../../components/infographic/infographic';
 import '../../components/quote/quote';
 import '../../components/gallery-slider/gallery-slider';
@@ -12,9 +13,6 @@ import '../../components/stages/stages';
 import '../../components/docs/docs';
 import '../../components/gallery/gallery';
 import '../../components/reviews/reviews';
-
-import '../../components/tags/tags';
-import '../../components/subsection-section/subsection-section';
 
 import '../../components/article-nav/article-nav';
 import '../../components/card-details/card-details';
