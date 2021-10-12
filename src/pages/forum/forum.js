2345161ddf400d5182a5a2e4f9dc8dffbfce5358
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-12 11:08:57 +0300

    update

--- src/pages/forum/forum.js
+++ src/pages/forum/forum.js
@@ -5,13 +5,15 @@ import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
 
+import '../../components/infographic/infographic';
+import '../../components/quote/quote';
 import '../../components/gallery-slider/gallery-slider';
 import '../../components/stages/stages';
 import '../../components/docs/docs';
-import '../../components/cards/cards';
 import '../../components/gallery/gallery';
 import '../../components/reviews/reviews';
 
+import '../../components/tags/tags';
 import '../../components/subsection-section/subsection-section';
 
 import '../../components/article-nav/article-nav';
