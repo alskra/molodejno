2a4e16d239830597b8634cc1c0f4f17994066286
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-19 16:08:16 +0300

    update

---
+++ src/pages/news/news.js
@@ -0,0 +1,17 @@
+import Alpine from 'alpinejs';
+
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/section-nav/section-nav';
+import '../../components/filters/filters';
+import '../../components/events-slider/events-slider';
+import '../../components/cards/cards';
+
+import '../../components/section/section';
+
+import '../../components/app/app';
+
+Alpine.start();
