bac2011bb06a0ee08c3bf38c3cc571a1464f244a
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-20 14:30:53 +0300

    update

--- src/pages/new/new.js
+++ src/pages/new/new.js
@@ -5,15 +5,13 @@ import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
 
+import '../../components/card-info/card-info';
+import '../../components/article-nav/article-nav';
 import '../../components/gallery-slider/gallery-slider';
 import '../../components/quote/quote';
 
 import '../../components/subsection-section/subsection-section';
-
-import '../../components/card-info/card-info';
-import '../../components/article-nav/article-nav';
 import '../../components/subsection/subsection';
-
 import '../../components/section/section';
 
 import '../../components/app/app';
