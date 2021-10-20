4a00e4db5140d5eaba1d18da96c0922a18591822
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-20 15:41:54 +0300

    update

---
+++ src/pages/playbill/playbill.js
@@ -0,0 +1,18 @@
+import Alpine from 'alpinejs';
+
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/events-slider/events-slider';
+import '../../components/filters/filters';
+import '../../components/cards/cards';
+
+import '../../components/subsection-section/subsection-section';
+import '../../components/subsection/subsection';
+import '../../components/section/section';
+
+import '../../components/app/app';
+
+Alpine.start();
