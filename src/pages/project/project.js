643f6e88918270350256941ac75206837fbedbf6
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-22 11:20:46 +0300

    update

---
+++ src/pages/project/project.js
@@ -0,0 +1,18 @@
+import Alpine from 'alpinejs';
+
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/article-nav/article-nav';
+import '../../components/card-details/card-details';
+import '../../components/infographic/infographic';
+
+import '../../components/subsection-section/subsection-section';
+import '../../components/subsection/subsection';
+import '../../components/article/article';
+
+import '../../components/app/app';
+
+Alpine.start();
