ae4f5047d1d7a36b075d99f86ac2fbecc24d206d
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-26 19:35:09 +0300

    update

---
+++ src/pages/release/release.js
@@ -0,0 +1,15 @@
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/card-info/card-info';
+import '../../components/article-nav/article-nav';
+import '../../components/gallery-slider/gallery-slider';
+import '../../components/gallery/gallery';
+
+import '../../components/subsection-section/subsection-section';
+import '../../components/subsection/subsection';
+import '../../components/section/section';
+
+import '../../components/app/app';
---
+++ src/pages/releases/releases.js
@@ -0,0 +1,14 @@
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/section-nav/section-nav';
+import '../../components/filters/filters';
+import '../../components/cards/cards';
+
+import '../../components/subsection-section/subsection-section';
+import '../../components/subsection/subsection';
+import '../../components/section/section';
+
+import '../../components/app/app';
