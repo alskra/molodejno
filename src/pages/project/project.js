5bf49cd2e4206e6b6aab6725872be06b4e9bb15b
alskra <krasnoyurchenko.a.s@gmail.com>
2021-10-26 16:27:34 +0300

    update

--- src/pages/project/project.js
+++ src/pages/project/project.js
@@ -8,6 +8,11 @@ import '../../components/card-details/card-details';
 import '../../components/infographic/infographic';
 import '../../components/gallery-slider/gallery-slider';
 import '../../components/stages/stages';
+import '../../components/docs/docs';
+import '../../components/gallery/gallery';
+import '../../components/reviews/reviews';
+import '../../components/partners/partners';
+import '../../components/contacts/contacts';
 
 import '../../components/subsection-section/subsection-section';
 import '../../components/subsection/subsection';
