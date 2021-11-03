ea554d9e9d0bd583b7a84cd19f117092a7b4e7c6
alskra <krasnoyurchenko.a.s@gmail.com>
2021-11-03 12:17:29 +0300

    update

--- src/components/activities/activities.js
+++ src/components/activities/activities.js
@@ -1,1 +1,15 @@
+import Alpine from 'alpinejs';
+import Masonry from 'masonry-layout';
 import './activities.scss';
+
+Alpine.data('activities', () => ({
+	masonry: undefined,
+	init() {
+		this.masonry = new Masonry(this.$refs.grid, {
+			itemSelector: '.activities__item',
+			gutter: '.activities__gap',
+		});
+
+		setTimeout(() => this.masonry.layout(), 500);
+	},
+}));
