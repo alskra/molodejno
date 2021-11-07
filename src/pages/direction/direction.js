36c12e9262f27ed64e0e6612d5d491c0f881ce10
alskra <krasnoyurchenko.a.s@gmail.com>
2021-11-07 14:27:22 +0300

    update

---
+++ src/pages/direction/direction.js
@@ -0,0 +1,11 @@
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/article-nav/article-nav';
+import '../../components/card-details/card-details';
+
+import '../../components/subsection-section/subsection-section';
+import '../../components/subsection/subsection';
+import '../../components/article/article';
