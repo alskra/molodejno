a3be40326fc16d94989e141e26add8902e7157c0
alskra <krasnoyurchenko.a.s@gmail.com>
2021-11-13 23:28:14 +0300

    update

---
+++ src/pages/contacts/contacts.js
@@ -0,0 +1,13 @@
+import '../../js/base';
+
+import '../../components/header/header';
+import '../../components/footer/footer';
+
+import '../../components/card-info/card-info';
+import '../../components/article-nav/article-nav';
+import '../../components/gallery-slider/gallery-slider';
+import '../../components/gallery/gallery';
+
+import '../../components/subsection-section/subsection-section';
+import '../../components/subsection/subsection';
+import '../../components/section/section';
