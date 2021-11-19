b6b7691b27d158ccfb62a456f6a001b10d3f7089
alskra <krasnoyurchenko.a.s@gmail.com>
2021-11-19 10:22:22 +0300

    update

--- src/components/form-feedback/form-feedback.js
+++ src/components/form-feedback/form-feedback.js
@@ -1,1 +1,53 @@
+import Alpine from 'alpinejs';
 import './form-feedback.scss';
+
+Alpine.data('formFeedback', () => ({
+	isValid: undefined,
+	isSubmit: false,
+	success: false,
+	error: null,
+	validate(obj) {
+		const target = obj.target || obj;
+		const valid = target.validity.valid;
+
+		target.classList.toggle('is-invalid', !valid);
+
+		return valid;
+	},
+	formValidate() {
+		this.isValid = true;
+
+		for (const item of this.$root.querySelectorAll('input, textarea')) {
+			if (!this.validate(item)) {
+				this.isValid = false;
+				break;
+			}
+		}
+	},
+	async submit(evt) {
+		evt.preventDefault();
+		this.formValidate();
+
+		if (this.isValid) {
+			try {
+				this.isSubmit = true;
+
+				const response = await fetch(this.$root.action, {
+					method: 'POST',
+					// body: new FormData(this.$root),
+				});
+
+				if (response.ok) {
+					this.error = null;
+					this.success = true;
+				} else {
+					this.error = `Ошибка HTTP: ${response.status} (${response.statusText})`;
+				}
+			} catch (error) {
+				this.error = error;
+			} finally {
+				this.isSubmit = false;
+			}
+		}
+	},
+}));
--- src/components/form-field/form-field.js
+++ src/components/form-field/form-field.js
@@ -7,6 +7,8 @@ Alpine.data('formField', () => ({
 	init() {
 		if (this.$root.type === 'tel') {
 			// this.$root.placeholder = '+7 (000) 000-00-00';
+			// eslint-disable-next-line
+			this.$root.pattern = '\\+7 \\([0-9]{3}\\) [0-9]{3}-[0-9]{2}-[0-9]{2}';
 			this.mask = new IMask(this.$root, {
 				mask: '+{7} (000) 000-00-00',
 				// lazy: false, // make placeholder always visible
