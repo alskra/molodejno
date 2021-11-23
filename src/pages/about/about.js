1a3d1ef19d31c84c61772524c79a90696d4baf1c
alskra <krasnoyurchenko.a.s@gmail.com>
2021-11-23 15:54:06 +0300

    update

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -3,9 +3,9 @@ import '../../js/base';
 import '../../components/header/header';
 import '../../components/footer/footer';
 
-import '../../components/section-promo/section-promo';
+import '../../components/gallery-slider/gallery-slider';
 import '../../components/section-feedback/section-feedback';
 
 import '../../components/subsection-section/subsection-section';
 import '../../components/subsection/subsection';
-import '../../components/section/section';
+import '../../components/section-promo/section-promo';
