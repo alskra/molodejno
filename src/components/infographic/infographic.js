e304df2ede3a4f0400ecb4ec2974e3ae93ef19f7
alskra <krasnoyurchenko.a.s@gmail.com>
2021-11-26 16:57:27 +0300

    update

--- src/components/infographic/infographic.js
+++ src/components/infographic/infographic.js
@@ -4,11 +4,47 @@ import Swiper from 'swiper';
 import '../score-animated/score-animated';
 import './infographic.scss';
 
-Alpine.data('infographic', () => ({
-	swiper: undefined,
-	init() {
-		this.swiper = new Swiper(this.$refs.container, {
-			slidesPerView: 'auto',
-		});
-	},
-}));
+Alpine.data('infographic', (onlyMobile = true) => {
+	let inst;
+
+	return {
+		swiper: undefined,
+		swiperIsInit: false,
+		get hasSwiper() {
+			return !(onlyMobile && this.$store.isDesktop);
+		},
+		init() {
+			inst = this;
+		},
+		destroy() {
+			inst.destroySwiper();
+		},
+		initSwiper() {
+			if (!this.swiperIsInit) {
+				this.swiper = new Swiper(this.$refs.swiper, {
+					slidesPerView: 'auto',
+					on: {
+						init: () => {
+							this.swiperIsInit = true;
+						},
+						destroy: () => {
+							this.swiperIsInit = false;
+						},
+					},
+				});
+			}
+		},
+		destroySwiper() {
+			if (this.swiperIsInit) {
+				this.swiper.destroy();
+			}
+		},
+		effect() {
+			if (this.hasSwiper) {
+				this.$nextTick(() => this.initSwiper());
+			} else {
+				this.destroySwiper();
+			}
+		},
+	};
+});
