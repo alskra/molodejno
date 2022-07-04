794e7b1ff7ed544d24e6f2c244f1040936900852
alskra <krasnoyurchenko.a.s@gmail.com>
2022-07-05 01:03:10 +0300

    update

--- src/js/viewport/scroll.js
+++ src/js/viewport/scroll.js
@@ -1,21 +1,28 @@
-export function getElFromHash(hash) {
+function getElFromHash(hash) {
 	return document.getElementById(hash.replace('#', ''));
 }
 
-export default function $({boundaryEl, spy = false} = {}) {
-	function getBoundary() {
-		return Math.floor(boundaryEl.getBoundingClientRect().bottom);
-	}
+function stripHash(url) {
+	url = new URL(url);
 
+	return url.href.replace(url.hash, '');
+}
+
+export default function $({boundaryEl = null, spy = false} = {}) {
+	$.boundaryEl = boundaryEl;
+
+	/**
+	 * Scroll handling when load
+	 */
 	document.documentElement.style.scrollBehavior = 'auto';
 
 	window.addEventListener('load', () => {
 		setTimeout(() => {
-			if (boundaryEl) {
+			if ($.boundaryEl) {
 				const targetEl = getElFromHash(location.hash);
 
 				if (targetEl && Math.floor(targetEl.getBoundingClientRect().top) === 0) {
-					scrollBy(0, -getBoundary());
+					scrollBy(0, -$.getBoundary());
 				}
 			}
 
@@ -23,7 +30,7 @@ export default function $({boundaryEl, spy = false} = {}) {
 		});
 	});
 
-	if (boundaryEl) {
+	if ($.boundaryEl) {
 		/**
 		 * Change hash or navigation with hash change is emit two events: `popstate` and `hashchange`
 		 */
@@ -31,20 +38,28 @@ export default function $({boundaryEl, spy = false} = {}) {
 		// 	const targetEl = getElFromHash(location.hash);
 		//
 		// 	if (targetEl) {
-		// 		scrollTo(0, scrollY + targetEl.getBoundingClientRect().top - getBoundary());
+		// 		$.scrollToTarget(targetEl);
 		// 	}
 		// });
 
+		/**
+		 * Anchor link handling
+		 */
 		document.addEventListener('click', (evt) => {
-			if (evt.target.tagName === 'A' &&
-				evt.target.href.replace(/#.*/, '') ===
-				location.href.replace(/#.*/, '')) {
+			if (evt.target.tagName === 'A' && stripHash(evt.target.href) === stripHash(location.href)) {
 				evt.preventDefault();
 
 				const targetEl = getElFromHash(evt.target.hash);
 
 				if (targetEl) {
-					scrollTo(0, scrollY + targetEl.getBoundingClientRect().top - getBoundary());
+					/**
+					 * Push a state if target element is existed
+					 */
+					if (evt.target.hash !== location.hash) {
+						history.pushState(null, null, evt.target.hash);
+					}
+
+					$.scrollToTarget(targetEl);
 				}
 			}
 		});
@@ -60,7 +75,7 @@ export default function $({boundaryEl, spy = false} = {}) {
 				let currentTarget;
 
 				document.querySelectorAll('[data-scroll]').forEach((targetEl) => {
-					if (Math.floor(targetEl.getBoundingClientRect().top) <= (boundaryEl ? getBoundary() : 0)) {
+					if (Math.floor(targetEl.getBoundingClientRect().top) <= $.getBoundary()) {
 						currentTarget = targetEl;
 					}
 				});
@@ -68,7 +83,7 @@ export default function $({boundaryEl, spy = false} = {}) {
 				if (currentTarget && `#${currentTarget.id}` !== location.hash) {
 					history.replaceState(null, null, `#${currentTarget.id}`);
 				} else if (!currentTarget) {
-					history.replaceState(null, null, location.href.replace(/#.*/, ''));
+					history.replaceState(null, null, stripHash(location.href));
 				}
 
 				window.dispatchEvent(new Event('scroll-hashchange'));
@@ -76,3 +91,12 @@ export default function $({boundaryEl, spy = false} = {}) {
 		}, {passive: true});
 	}
 }
+
+Object.assign($, {
+	getBoundary() {
+		return ($.boundaryEl || 0) && Math.floor($.boundaryEl.getBoundingClientRect().bottom);
+	},
+	scrollToTarget(targetEl) {
+		scrollTo(0, scrollY + targetEl.getBoundingClientRect().top - $.getBoundary());
+	},
+});
